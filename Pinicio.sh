ff372394f1f53202eeb6b94e063400bce97e20f5
root <fcojriosbello@gmail.com>
2015-12-28 22:50:11 +0100

    Added Docker to init script

---
+++ Pinicio.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#######################################################
+#         Preparando e instalando Ansible             #
+#######################################################
+
+cd /etc
+# cloning repository to build it from source
+git clone git://github.com/ansible/ansible.git --recursive
+cd ansible
+make rpm
+cd rpm-build
+# install some dependencies
+yum  -y install python-devel.x86_64 PyYAML python-httplib2 python-jinja2 python-keyczar python-paramiko python-six sshpass
+rpm -Uvh ansible-*.noarch.rpm
+yum -y install docker docker-registry
