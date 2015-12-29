f604cf1fd9e52bad0ae8f82a9b5724585097d791
root <fcojriosbello@gmail.com>
2015-12-30 00:54:31 +0100

    Deployed on a single node

--- Pinicio.sh
+++ Pinicio.sh
@@ -13,8 +13,3 @@ cd rpm-build
 # install some dependencies
 yum  -y install python-devel.x86_64 PyYAML python-httplib2 python-jinja2 python-keyczar python-paramiko python-six sshpass
 rpm -Uvh ansible-*.noarch.rpm
-#yum -y install docker docker-registry #ruby rubygems
-#gem update --system 
-#gem install vagrant
-#wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.rpm
-#yum -y install VirtualBox-5.0 install vagrant_1.8.1_x86_64.rpm
