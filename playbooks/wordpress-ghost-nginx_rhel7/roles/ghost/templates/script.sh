245eebcf7dca9f5b142648c62a92e5a2c90c8e10
root <fcojriosbello@gmail.com>
2015-12-31 15:24:59 +0100

    Refactor

--- playbooks/wordpress-ghost-nginx_rhel7/roles/ghost/templates/script.sh
+++ playbooks/wordpress-ghost-nginx_rhel7/roles/ghost/templates/script.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-
-cat /var/log/audit/audit.log | grep nginx | grep denied | audit2allow -M mynginx
-semodule -i mynginx.pp
+printf '%s\n%s\n' '192.168.1.35 staghost.com' '192.168.1.35 stawordpress.com' >> /etc/hosts
+curl staghost.com
+sudo cat /var/log/audit/audit.log | grep nginx | grep denied | audit2allow -M mynginx
+sudo semodule -i mynginx.pp
